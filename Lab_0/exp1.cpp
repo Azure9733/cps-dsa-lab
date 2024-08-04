0d5fdc9b6a650921d25e54e616e8688c44183ce9
Azure <143328010+Azure9733@users.noreply.github.com>
2024-08-04 07:20:20 +0000

    Add files via upload

---
+++ Lab_0/exp1.cpp
@@ -0,0 +1,10 @@
+// Write a C++ program to display the following:
+// Enjoy yourself with C++!
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Enjoy yourself with C++!" << endl;
+	return 0;
+}
